bf6472a64a233b8ec8c66f63ca6b64482b79219a
Sascha Holzhauer <sascha.holzhauer@ed.ac.uk>
2014-04-25 23:10:49 +0100

    :: Bugfix: PRasterWriter/Raster (NDATA not initialised) minor change: comments in PRasterWriter, ABMPerister

--- src/org/volante/abm/output/PRasterWriter.java
+++ src/org/volante/abm/output/PRasterWriter.java
@@ -10,40 +10,57 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+import org.apache.log4j.Logger;
+
 import com.moseph.gis.raster.Raster;
+import com.moseph.gis.raster.RasterWriter;
+
 
 /**
+ * Note: The Raster class is not well implemented. Calling {@link Raster#getNDATA()} without a
+ * previous call to {@link Raster#setNDATA(String)} may cause a segmentation fault since the object
+ * it returns has not been initialised.
+ * 
+ * Based on {@link RasterWriter} (Dave Murray-Rust)
+ * 
  * @author Sascha Holzhauer
- *
+ * 
  */
 public class PRasterWriter {
+
+	/**
+	 * Logger
+	 */
+	static private Logger				logger			= Logger.getLogger(PRasterWriter.class);
 
 	NumberFormat						cellFormat		= null;
 	String								nodataString	= Raster.DEFAULT_NODATA;
 	public static final DecimalFormat	INT_FORMAT		= new DecimalFormat("0");
 
 	public void writeRaster(String filename, Raster r) throws IOException {
+		r.setNDATA("null");
+
 		File f = new File(filename);
 		if (f.exists()) {
 			f.delete();
 		}
 		if (!f.createNewFile()) {
-			System.err.println("Could not create file for some reason!");
+			logger.error("Could not create file for some reason!");
 		}
+		String linebreak = System.getProperty("line.separator");
 		FileWriter o = new FileWriter(f);
 		o.write("ncols " + r.getCols());
-		o.write("\n");
+		o.write(linebreak);
 		o.write("nrows " + r.getRows());
-		o.write("\n");
+		o.write(linebreak);
 		o.write("xllcorner " + r.getXll());
-		o.write("\n");
+		o.write(linebreak);
 		o.write("yllcorner " + r.getYll());
-		o.write("\n");
+		o.write(linebreak);
 		o.write("cellsize " + r.getCellsize());
-		o.write("\n");
-		// this hack is required since writing NULL results in some kind of segmentation fault would
-		// when running with MPI on eddie:
-		o.write("NODATA_value " + r.getNDATA() != null ? r.getNDATA() : "null");
+		o.write(linebreak);
+		o.write("NODATA_value " + r.getNDATA());
+		o.write(linebreak);
 
 		for (double[] row : r.getData()) {
 			StringBuffer b = new StringBuffer();
