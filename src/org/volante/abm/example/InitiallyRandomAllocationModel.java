0c6aabe2807eaacf6b35836ec6feeafed6d50716
S-Holzhauer <S-Holzhauer@localhost>
2015-12-15 23:26:43 +0000

    :: CRAFTY_Parallel :: v0.1.31 | InitiallyRandomAllocationModel - set version to 0.1.31 - feature added: InitiallyRandomAllocationModel (+test) - major change: renamed NormalisedAdaptingCurveCompetitivenessModel

---
+++ src/org/volante/abm/example/InitiallyRandomAllocationModel.java
@@ -0,0 +1,162 @@
+/**
+ * This file is part of
+ * 
+ * CRAFTY - Competition for Resources between Agent Functional TYpes
+ *
+ * Copyright (C) 2015 School of GeoScience, University of Edinburgh, Edinburgh, UK
+ * 
+ * CRAFTY is free software: You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * CRAFTY is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * School of Geoscience, University of Edinburgh, Edinburgh, UK
+ * 
+ * Created by Sascha Holzhauer on 20 Nov 2015
+ */
+package org.volante.abm.example;
+
+
+import static com.moseph.modelutils.Utilities.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementMap;
+import org.volante.abm.agent.Agent;
+import org.volante.abm.agent.GeoAgent;
+import org.volante.abm.agent.PotentialAgent;
+import org.volante.abm.agent.SocialAgent;
+import org.volante.abm.data.Cell;
+import org.volante.abm.data.ModelData;
+import org.volante.abm.data.Region;
+import org.volante.abm.models.AllocationModel;
+import org.volante.abm.models.utils.GivingInStatisticsMessenger;
+import org.volante.abm.models.utils.TakeoverMessenger;
+import org.volante.abm.models.utils.TakeoverObserver;
+import org.volante.abm.output.GivingInStatisticsObserver;
+import org.volante.abm.param.RandomPa;
+import org.volante.abm.schedule.RunInfo;
+
+/**
+ * @author Sascha Holzhauer
+ *
+ */
+public class InitiallyRandomAllocationModel implements AllocationModel, TakeoverMessenger, GivingInStatisticsMessenger {
+
+	/**
+	 * Logger
+	 */
+	static private Logger logger = Logger.getLogger(InitiallyRandomAllocationModel.class);
+
+	@Element(required = true)
+	protected AllocationModel regularAllocModel = null;
+
+	
+	@ElementMap(entry = "proportion", key = "aft", attribute = true, inline = true, required = false)
+	protected Map<String, Double> aftProportions = new HashMap<>();
+	
+	Map<PotentialAgent, Double> aftScores = new HashMap<>();
+	
+	protected boolean networkNullErrorOccurred = false;
+
+	/**
+	 * @see org.volante.abm.serialization.Initialisable#initialise(org.volante.abm.data.ModelData,
+	 *      org.volante.abm.schedule.RunInfo, org.volante.abm.data.Region)
+	 */
+	@Override
+	public void initialise(ModelData data, RunInfo info, Region extent) throws Exception {
+		this.regularAllocModel.initialise(data, info, extent);
+		
+		for (PotentialAgent pa : extent.getPotentialAgents()) {
+			aftScores.put(pa, aftProportions.get(pa.getID()));
+		}
+	}
+
+	/**
+	 * @see org.volante.abm.models.AllocationModel#allocateLand(org.volante.abm.data.Region)
+	 */
+	@Override
+	public void allocateLand(Region r) {
+		if (r.getRinfo().getSchedule().getCurrentTick() == r.getRinfo().getSchedule().getStartTick()) {
+			this.allocateLandRandomly(r);
+		} else {
+			this.regularAllocModel.allocateLand(r);
+		}
+	}
+
+	/**
+	 * @param r
+	 */
+	protected void allocateLandRandomly(Region r) {
+		for (Cell c : r.getAllCells()) {
+
+			PotentialAgent pa =
+					sample(aftScores, false, r.getRandom().getURService(), RandomPa.RANDOM_SEED_RUN_ALLOCATION.name());
+
+			if (pa != null) {
+				Agent agent = pa.createAgent(r);
+
+				r.setOwnership(agent, c);
+
+				if (r.getNetworkService() != null) {
+					// <- LOGGING
+					if (logger.isDebugEnabled()) {
+						logger.debug("Linking agent " + agent);
+					}
+					// LOGGING ->
+
+					if (r.getNetwork() != null) {
+						if (r.getGeography() != null && agent instanceof GeoAgent) {
+							((GeoAgent) agent).addToGeography();
+						}
+						r.getNetworkService().addAndLinkNode(r.getNetwork(), (SocialAgent) agent);
+					} else {
+						if (!networkNullErrorOccurred) {
+							logger.warn("Network object not present during creation of new agent (subsequent error messages are suppressed)");
+							networkNullErrorOccurred = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @see org.volante.abm.models.AllocationModel#getDisplay()
+	 */
+	@Override
+	public AllocationDisplay getDisplay() {
+		return regularAllocModel.getDisplay();
+	}
+
+	/**
+	 * @see org.volante.abm.models.utils.GivingInStatisticsMessenger#registerGivingInStatisticOberserver(org.volante.abm.output.GivingInStatisticsObserver)
+	 */
+	@Override
+	public void registerGivingInStatisticOberserver(GivingInStatisticsObserver observer) {
+		if (this.regularAllocModel instanceof GivingInStatisticsMessenger) {
+			((GivingInStatisticsMessenger) this.regularAllocModel).registerGivingInStatisticOberserver(observer);
+		}
+	}
+
+	/**
+	 * @see org.volante.abm.models.utils.TakeoverMessenger#registerTakeoverOberserver(org.volante.abm.models.utils.TakeoverObserver)
+	 */
+	@Override
+	public void registerTakeoverOberserver(TakeoverObserver observer) {
+		if (this.regularAllocModel instanceof TakeoverMessenger) {
+			((TakeoverMessenger) this.regularAllocModel).registerTakeoverOberserver(observer);
+		}
+
+	}
+}
